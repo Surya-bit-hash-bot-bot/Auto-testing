f25db259f5e0e95630ca2ac97e308b3c440588ce
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-08 09:57:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "21740783")
+    API_HASH  = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7435219753:AAFzGs09ktzVU6Zt7NI_ChfVyAAPlDDe9FU") 
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","Speedwolf1")     
