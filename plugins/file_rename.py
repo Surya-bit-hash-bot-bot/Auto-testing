974d64bf6ec9b1084673d78616e72682228f108e
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-08 10:05:17 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -9,7 +9,7 @@ from helper.utils import progress_for_pyrogram, humanbytes, convert
 from helper.database import AshutoshGoswami24
 from config import Config, Txt
 from config import Config
-from queue import RenameQueue  # Import the queue functionality
+from queue import rename_queue  # Import the queue functionality
 import os
 import asyncio
 import time
