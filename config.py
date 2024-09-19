963881615c82a24df21283fe4e9b84c35d6f3ad5
Baji-kun <152496758+Baji-kun@users.noreply.github.com>
2024-09-20 01:10:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,9 +37,9 @@ class Txt(object):
     
 ➻ Use /tutorial Command To Know How To Use Me
 
-<b>Bot Is Made By @Itz_Spike</b>
+<b>Bot Is Made By @VOATcb</b>
 
-<b><a href='https://t.me/Netflix_Back_Up'>Movie & Series Channel</a></b>
+<b><a href='https://t.me/Manga_Yugen'>Yugen</a></b>
 """
     
     FILE_NAME_TXT = """<b><u>SETUP AUTO RENAME FORMAT</u></b>
@@ -57,16 +57,16 @@ Use These Keywords To Setup Custom File Name
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Anime_Wide'>Anime Channel</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/Itz_Spike'>Spike</a>
+<b>📢 Channel :</b> <a href='https://t.me/Manga_Yugen'>Manga Yugen</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/VOATcb'>Vot</a>
     
-<b>♻️ Bot Made By :</b> @Itz_Spike"""
+<b>♻️ Bot Made By :</b> @VOATcb"""
     SEND_METADATA = """
 ❪ SET CUSTOM METADATA ❫
 
 ☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
 
-◦ <code> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Anime_Warrior_Tamil" -metadata author="@Anime_Warrior_Tamil" -metadata:s:s title="Subtitled By :- @Anime_Warrior_Tamil" -metadata:s:a title="By :- @Anime_Warrior_Tamil" -metadata:s:v title="By:- @Anime_Warrior_Tamil" </code>"""
+◦ <code> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Manga_Yugen" -metadata author="@Manga_Yugen" -metadata:s:s title="Subtitled By :- @Manga_Yugen" -metadata:s:a title="By :- @Manga_Yugen" -metadata:s:v title="By:- @Manga_Yugen" </code>"""
 
 
     
@@ -89,7 +89,7 @@ Use These Keywords To Setup Custom File Name
 ┣⪼ ⏳️ Dᴏɴᴇ : {0}%
 ┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
 ┣⪼ ⏰️ Eᴛᴀ: {4}
-┣⪼ 😶‍🌫️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ: @Netflix_Dual
+┣⪼ 😶‍🌫️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ: @Manga_Yugen
 ╰━━━━━━━━━━━━━━━➣ </b>"""
     
     
@@ -97,11 +97,11 @@ Use These Keywords To Setup Custom File Name
     
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 1 Rs Upto Your Choice.
     
-<b>UPI diya to kya pata tu police ka sath share nhi karega 😶‍🌫️- </b> """
+<b>Dm - @VOATcb</b> """
     
     HELP_TXT = """<b>Hey</b> {}
     
-@Itz_Spike """
+ https://t.me/Manga_Yugen_Group"""
 
 
 
